91f356aca5449f1dc13ea7b5b5ff6aa7c50aabbc
Gaëtan Dechappe <gaetandec01@gmail.com>
2022-03-17 16:08:18 +0000

    Update code/controleur/login/.gitkeep, code/controleur/login/login.php

---
+++ code/controleur/login/login.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Contrôleur de la partie login
+ */
+function login ()
+    {
+    global $vueListe;
+    
+    if (!isset($_GET["user"])) echo file_get_contents($vueListe["formLogin"]);
+    else
+        {
+        //Partie supérieure page (HEAD + haut du BODY)
+        echo file_get_contents($vueListe["entete"]);
+        
+        echo file_get_contents($vueListe["accueil"]);
+
+        //Partie inférieure page (bas du BODY + fin HTML)
+        //echo file_get_contents($viewList["foot"]);
+        
+        
+        }
+    }
+?>
+
