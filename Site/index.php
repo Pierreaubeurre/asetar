b270d4593b6958639bea1a6db5dbd291bd7c5e0a
Thibault COLLART <tybo2803@hotmail.fr>
2022-04-05 12:58:54 +0000

    Téléverser un nouveau fichier

---
+++ Site/index.php
@@ -0,0 +1,19 @@
+<?php
+#$lifetime=0;
+#session_set_cookie_params($lifetime);
+session_start();
+
+require_once "init/init.php";
+
+if(isset($_GET["page"])) 
+{
+	$page=$_GET["page"];
+}
+else 
+{
+	$page="accueil"; //sinon page d'accueil par défaut
+}
+
+
+mainControl($page);
+?>
