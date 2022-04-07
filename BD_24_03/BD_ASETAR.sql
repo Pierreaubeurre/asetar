971fb6b6e9b4b2c45d8a44d4ec84974ade30c0b8
Ryan TEIXEIRA <wikop27@gmail.com>
2022-04-07 07:34:01 +0000

    Replace BD_ASETAR.sql

--- BD_24_03/BD_ASETAR.sql
+++ BD_24_03/BD_ASETAR.sql
@@ -24,9 +24,9 @@ CREATE TABLE COMPTE
 
 CREATE TABLE PROFIL
 	(
-      ID_U      INTEGER     NOT NULL AUTO_INCREMENT,
-      NOM   VARCHAR(20)     NULL,
+      ID_U      INTEGER     NOT NULL,
       PRENOM  VARCHAR(20)    NULL,
+      NOM   VARCHAR(20)     NULL,
       PSEUDO_U    VARCHAR(20)    NULL,
       AGE    INTEGER        NOT NULL,
       BIO    VARCHAR(200)    NULL,
@@ -48,6 +48,7 @@ CREATE TABLE BOUTIQUE
       ARTICLE VARCHAR(30)    NULL,
       PRIX INTEGER    NOT NULL,
       STOCK INTEGER     NOT NULL,
+      IMAGE VARCHAR(200)    NULL,
       PRIMARY KEY (ARTICLE)
     );
 
@@ -63,3 +64,9 @@ INSERT INTO COMPTE VALUES (7, "Lumiere",     "sombre",       "User");
 INSERT INTO COMPTE VALUES (8, "Test",        "test2022",     "User");
 
 INSERT INTO PROFIL VALUES (4, "Ryan", "TEIXEIRA", "Aeklys", "18", "Jeune-entrepreneur", "08000", "2003-05-27", "06-06-06-06-06", "BTS SIO MONGE", "Français",  "ryan.txa27@gmail.com");
+
+INSERT INTO BOUTIQUE VALUES ("Mug",          "4.99",   "99",    "");
+INSERT INTO BOUTIQUE VALUES ("T-shirt",     "9.99",   "99",    "");
+INSERT INTO BOUTIQUE VALUES ("Bracelet",     "1.99",   "99",    "");
+INSERT INTO BOUTIQUE VALUES ("Sac à dos",   "14.99",   "99",    "");
+INSERT INTO BOUTIQUE VALUES ("Stickers",     "4.99",   "99",    "");
