08ab5ea9cd71f0cc822e2699625c7ae9eefac935
Thibault COLLART <tybo2803@hotmail.fr>
2022-04-26 11:30:57 +0000

    Téléverser un nouveau fichier

---
+++ controleur/controleurPrincipal.php
@@ -0,0 +1,36 @@
+<?php
+
+//Liste des pages possibles
+require_once "parametre/pageListe.php";
+
+function mainControl (string $page="accueil")//A modifier si on rajoute des pages
+{
+	global $action;//importe le tableau de la pageListe.php
+	switch ($page)
+	{
+		case "accueil"	:	require_once $action["accueil"];
+					affAccueil();
+					break;
+		case "profile" :	require_once $action["visuProfile"];
+					afficheProfile();
+					break;
+		case "connexion":	require_once $action["connexion"];
+					login();
+					break;
+		case "inscription":	require_once $action["inscription"];
+					break;
+        case "deconnexion": require_once $action["deconnexion"];
+                    deco();
+                    break;
+        case "modifProfil":	require_once $action["modifProfil"];
+                    modifProfil();
+					afficheProfile();
+					break;
+        case "boutique": require_once $action["boutique"];
+                    afficheBoutique();
+                    break;
+	}
+}
+	
+?>
+
